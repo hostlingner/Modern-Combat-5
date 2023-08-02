18da5e797b230831ff7524aea3281f7741ee87f3
hostlingner <117482363+hostlingner@users.noreply.github.com>
2023-08-02 11:47:51 -0300

    Update Cliente - Khalid.lua

--- Cliente - Khalid.lua
+++ Cliente - Khalid.lua
@@ -1,1 +1,1 @@
-gg.aler("Kicked: You have been banned from this hack\nReason: sold our products to other players or shared the hack.")
+gg.alert("Kicked: You have been banned from this hack\nReason: sold our products to other players or shared the hack.")
